e6cb45c4ed7fa07ca01fa3229e1563fb02e3bde2
Agustin Morinigo <agustinmorinigo1999@gmail.com>
2025-05-25 00:07:59 -0300

    Feature: Add Prisma

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,46 @@
 import express from 'express';
-import authRoutes from './routes/auth/index.js';
+import prisma from "@/script.js"
 
 // Config.
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Auth routes.
-// app.use('/login', authRoutes)            // TO DO: loginRoutes.
-app.post('/login', (req, res) => { res.send("jeeeeeeeeeeeeeee") })            // TO DO: loginRoutes.
-// app.use('/register', authRoutes)         // TO DO: registerRoutes.
-// app.use('/recover-password', authRoutes) // TO DO: recoveryPasswordRoutes.
-// app.use('/logout', authRoutes)           // TO DO: logoutRoutes.
+app.post("/create", async (req, res) => {
+  console.log("ENTRÓ ACÁ.");
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: "aguuuu@agu4.com",
+        name: "Aguuuuu4",
+        posts: {
+          create: {
+            title: "Título del post 4444",
+            content: "Hola soy el contenidooo.",
+            published: false,
+          }
+        }
+      }
+    })
+
+    console.log("USERRRRRR: ", user);
+    res.send({ status: 201, message: "Usuario creado" })
+
+  } catch (error) {
+    console.log("EERROR: ", error);
+    res.send({ status: 400, message: "Algo salió MAL........" })
+  }
+})
+
+app.get("/users", async (req, res) => {
+
+  try {
+    const users = await prisma.user.findMany()
+    res.send({ status: 200, description: { users } })
+  } catch (error) {
+    res.send({ status: 400, message: "Algo salió MAL........" })
+  }
+
+})
 
 // Run app.
 app.listen(PORT)
